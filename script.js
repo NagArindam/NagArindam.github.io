be689e4958d9a2550d275e3a1e37fc396e6dc52a
NagArindam <inspkolkata12@gmail.com>
2022-12-29 01:16:49 +0530

    task1 completed

---
+++ script.js
@@ -0,0 +1,7 @@
+//javascript for navigation bar effects on scroll
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+  });
+  
+  
